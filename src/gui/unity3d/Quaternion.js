c326545b6b9aab504af5f0c4a09ca61f414e2778
Luke Zhou <153415592@qq.com>
2016-12-10 19:39:12 +0800

    Quaternion from euler

--- src/gui/unity3d/Quaternion.js
+++ src/gui/unity3d/Quaternion.js
@@ -3,10 +3,22 @@ UnityEngine.Quaternion = function ( x, y, z, w ) {
 	this.x = x || 0;
 	this.y = y || 0;
 	this.z = z || 0;
-	this.w = w || 0;
+	this.w = w || 1;
 };
 
 UnityEngine.Quaternion.prototype = {
 	constructor: UnityEngine.Quaternion,
 	set: function(x, y, z, w) { this.x = x; this.y = y; this.z = z; this.w = w; },
+	euler: function(x, y, z) {
+		var c1 = Math.cos( x * 0.5 );
+		var c2 = Math.cos( y * 0.5 );
+		var c3 = Math.cos( z * 0.5 );
+		var s1 = Math.sin( x * 0.5 );
+		var s2 = Math.sin( y * 0.5 );
+		var s3 = Math.sin( z * 0.5 );
+		this.x = s1 * c2 * c3 + c1 * s2 * s3;
+		this.y = c1 * s2 * c3 - s1 * c2 * s3;
+		this.z = c1 * c2 * s3 + s1 * s2 * c3;
+		this.w = c1 * c2 * c3 - s1 * s2 * s3;
+	},
 };
--- src/gui/unity3d/Transform.js
+++ src/gui/unity3d/Transform.js
@@ -21,7 +21,7 @@ Object.assign(UnityEngine.Transform.prototype, UnityEngine.Component.prototype, 
     constructor: UnityEngine.Transform,
     Load: function(json) {
         if (json.t) this.localPosition.set(json.t.x, json.t.y, json.t.z);
-        if (json.r) this.localRotation.set(json.r.x, json.r.y, json.r.z, json.r.w);
+        if (json.r) this.localRotation.euler(json.r.x, json.r.y, json.r.z);
         if (json.s) this.localScale.set(json.s.x, json.s.y, json.s.z);
     },
     TransformPoint: function(pos) {
