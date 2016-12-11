24c90e6f5c150f5a577fecfdd7c742bba2b9b9d2
Luke Zhou <153415592@qq.com>
2016-12-11 23:00:00 +0800

    整理输入文件build

--- build/build.bat
+++ build/build.bat
@@ -3,7 +3,7 @@ call:combine > ..\src\ngui.js
 GOTO:EOF
 :combine
 echo // autogen by combine tools.
-for /f "tokens=*" %%i in (files.txt) do call:print_file "%%i"
+for /f "tokens=*" %%i in (files.txt) do call:print_file ..\src\gui\%%i
 GOTO:EOF
 :print_file
 echo.
