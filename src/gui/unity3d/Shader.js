6f2162ea00682331b2bd1fbcb6963f1bc25de56b
Luke Zhou <153415592@qq.com>
2016-12-11 11:48:42 +0800

    shader编译整理

--- src/gui/unity3d/Shader.js
+++ src/gui/unity3d/Shader.js
@@ -10,6 +10,67 @@ UnityEngine.Shader = function(json) {
     this.Blend = json.Blend | ['SrcAlpha', 'OneMinusSrcAlpha'];
 	this.vertexShader = json.vertexShader | 'void main() {\n\tgl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );\n}';
 	this.fragmentShader = json.fragmentShader | 'void main() {\n\tgl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );\n}';
+    this.program = null;
+
+	function fetchAttributeLocations( gl, program ) {
+		var attributes = {};
+		var n = gl.getProgramParameter( program, gl.ACTIVE_ATTRIBUTES );
+		for ( var i = 0; i < n; i ++ ) {
+			var info = gl.getActiveAttrib( program, i );
+			var name = info.name;
+			attributes[ name ] = gl.getAttribLocation( program, name );
+		}
+		return attributes;
+	}
+
+    function compileSource(gl, type, src) {
+		var shader = gl.createShader( type );
+		gl.shaderSource( shader, src );
+		gl.compileShader( shader );
+		if ( gl.getShaderParameter( shader, gl.COMPILE_STATUS ) === false )
+			console.error( 'THREE.WebGLShader: Shader couldn\'t compile.' );
+		return shader;
+    }
+
+    function compileShader(gl, vertexShader, fragmentShader) {
+		var program = gl.createProgram();
+        var glVertexShader = compileSource(gl, gl.VERTEX_SHADER, vertexShader);
+        var glFragmentShader = compileSource(gl, gl.FRAGMENT_SHADER, fragmentShader);
+		gl.attachShader(program, glVertexShader);
+		gl.attachShader(program, glFragmentShader);
+		gl.linkProgram( program );
+		if (gl.getProgramParameter(program, gl.LINK_STATUS) === false) {
+            var programLog = gl.getProgramInfoLog(program);
+            var vertexLog = gl.getShaderInfoLog(glVertexShader);
+            var fragmentLog = gl.getShaderInfoLog(glFragmentShader);
+			console.error('THREE.WebGLProgram: shader error: ', 
+                gl.getError(), 
+                'gl.VALIDATE_STATUS',
+                gl.getProgramParameter(program, gl.VALIDATE_STATUS),
+                'gl.getProgramInfoLog', 
+                programLog,
+                vertexLog, 
+                fragmentLog);
+        }
+		gl.deleteShader(glVertexShader);
+		gl.deleteShader(glFragmentShader);
+        return program;
+    }
+
+    function getUniforms(gl, program) {
+        var n = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+        for ( var i = 0; i !== n; ++ i ) {
+            var info = gl.getActiveUniform(program, i);
+            var path = info.name;
+            var addr = gl.getUniformLocation(program, path);
+            parseUniform(info, addr, this);
+        }
+    }
+
+    this.destroy = function() {
+        gl.deleteProgram(this.program);
+        this.program = undefined;
+    }
 };
 
 UnityEngine.Shader.prototype = {
